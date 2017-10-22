6fe160befa005f66520d3fb6e4cb9d8bd5378895
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-10-22 10:54:45 -0700

    Add missing whitespace

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
       noScript: {
         tag:     'noscript',
         content: "<p>" +
-                    "For full functionality of this site it is necessary to enable JavaScript." +
+                    "For full functionality of this site it is necessary to enable JavaScript. " +
                     "Here are the <a href='https://www.enable-javascript.com/' target='_blank'>" +
                     "instructions on how to enable JavaScript in your web browser</a>." +
                   "</p>"
